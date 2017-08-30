eab48b083807e492463f29c5e115b2ce2d704efb
David Milliner <david.milliner@inwebo.com>
2017-08-30 15:18:54 -0700

    Secon commit last version of U-Focus

---
+++ app/src/main/java/milliner/hc/SettingsFragment.java
@@ -0,0 +1,49 @@
+package milliner.hc;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+
+/**
+ * Created by davidmilliner on 12/06/16.
+ */
+public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.settings);
+    }
+
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        //IT NEVER GETS IN HERE!
+        if (key.equals("user_name"))
+        {
+            // Set summary to be the user-description for the selected value
+            Preference exercisesPref = findPreference(key);
+            exercisesPref.setSummary(sharedPreferences.getString(key, ""));
+        }
+        if (key.equals("game_mode"))
+        {
+            // Set summary to be the user-description for the selected value
+            Preference exercisesPref = findPreference(key);
+            exercisesPref.setSummary(sharedPreferences.getString(key, ""));
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+
+    }
+
+    @Override
+    public void onPause() {
+        getPreferenceManager().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+        super.onPause();
+    }
+}
